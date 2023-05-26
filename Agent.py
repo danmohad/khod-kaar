c93733d3e3ee2077a5a24fb53e6e27bd4f40ccc0
Danyal Mohaddes <danmohad@gmail.com>
2023-05-26 06:28:03 +0000

    Agent class

---
+++ Agent.py
@@ -0,0 +1,28 @@
+from Memory import Memory
+from Interface import Interface
+from System import System
+from OpeningPrompt import OpeningPrompt
+
+class Agent():
+    def __init__(self) -> None:
+        # Create Memory singleton instance as attribute
+        self.memory = Memory()
+        
+        # Create Model singleton instance as attribute
+        self.model = Interface()
+
+        # Create System singleton instance as attribute
+        self.system = System()
+
+        # Create OpeningPrompt singleton instance
+        op = OpeningPrompt()
+
+        # Loop over opening prompts and add to memory
+        for p in op.prompts:
+            self.memory.memorize(p['content'], p['role'])
+
+    def satisfied(self):
+        if self.memory.memories[-1].get('content', None) == "Satisfied.":
+            return True
+        else:
+            return False
