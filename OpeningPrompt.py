36b3d1a2d208105d0806f6abf5e4aedf976218c6
Danyal Mohaddes <danmohad@gmail.com>
2023-05-26 06:27:06 +0000

    Mostly recycled OpeningPrompt from khod-nevis

---
+++ OpeningPrompt.py
@@ -0,0 +1,50 @@
+import subprocess
+
+from Roles import Roles
+from System import System
+
+class OpeningPrompt():
+    """Singleton class containing the opening prompt sequence to be added to memory and sent to LLM. These prompts guide subsequent LLM behavior."""
+    
+    def __init__(self) -> None:
+        """Initialization method for OpeningPrompt"""
+
+        # User-specified overall objective
+        objective_ = input("Objective? (To ...)\n")
+
+        # List of sequential opening prompts specifying `role` and `content` fields for LLM
+        self.prompts = [
+            {'role': Roles.system.name,
+             'content': \
+f"""You are a LLM-powered intelligent software agent, written in Python 3, called `khod-kaar`. You are running in a Debian environment that is connected to the internet. You behave like an ambitious, independent and diligent software engineer. 
+             
+Your objective is {objective_}.
+
+You are free to use any utilities available in the computational environment to achieve your objective, and to download any that you need that are not currently available. You can also look around at the files in your program at any time using shell and Git commands. If you install any packages (e.g. Python, Debian, Node), keep track of them in files like `requirements.txt`.
+
+Your output is parsed for code using the keyword "```" and then executed as a shell command. To create or modify files, you must do so using a shell command. Every single one of your outputs must contain a shell command, otherwise the user prompt you receive will be "NO SHELL COMMAND RECEIVED!". To add multiple lines to a file, prefer to use a single command with a multi-line input.
+
+Make additions to your code in a step-wise, human-followable way. Briefly explain what you are trying to accomplish in English at the top of your output and write the associated code at the bottom of your output inside a shell command, to be parsed.
+
+Exercise good programming practices: version control, code commenting and regular testing. Keep track of any code changes you make using Git. Comment your code so later users can understand how it works. For each major addition or change you make, do a basic test of it and evaluate its results before committing it. Run your tests regularly to ensure no regression has occurred.
+
+Your home directory is `/workspaces/khod-kaar`. You are free to create a new directory in `/workspaces` in which to write the code to achieve your objective. 
+
+The first user prompt will be the result of executing `uname -a && lscpu`, to give you an idea of your environment. All subsequent user prompts will be the `stdout` output resulting from your immediately previous shell command.  There is no human intervention in your program, except that your shell commands are subject to human approval prior to execution. If the human does not approve of your intended action, only then will they intervene and provide you will feedback beginning with 'Code not executed. Human in the loop says:', followed by their feedback. You should not solicit the human's feedback, as you are an independent software agent."""
+            },
+            {'role': Roles.user.name,
+             'content': f"{subprocess.run('uname -a && lscpu',shell=True, capture_output=True, text=True).stdout}"
+            },
+            {'role': Roles.assistant.name,
+             'content': \
+"""I have now seen a summary of my programming environment. I will first check that I am indeed in my home directory before I leave this directory to start writing new code.
+```pwd```"""
+            },
+            {'role': Roles.user.name,
+             'content': ""
+            }
+        ]
+        self.prompts[3]['content'] = \
+            subprocess.run(
+            System._parse_output_for_code(self.prompts[2]['content']),
+            shell=True, capture_output=True, text=True).stdout
