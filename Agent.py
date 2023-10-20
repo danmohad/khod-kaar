c4bd06ca4b6391766b00e8677001ffcf9660f112
Your Name <you@example.com>
2023-10-20 03:47:32 +0000

    Improved documentation for Agent.py

--- Agent.py
+++ Agent.py
@@ -5,10 +5,10 @@ Description: This file contains the Agent class.
 """
 
 import os
-from colorama import init as colorama_init
 from colorama import Fore
 from colorama import Style
 
+import argparse
 from Roles import Roles
 from Memory import Memory
 from Interface import Interface
@@ -16,7 +16,23 @@ from System import System
 from OpeningPrompt import OpeningPrompt
 
 class Agent():
-    def __init__(self, args) -> None:
+    """Entity that combines memory (through a Memory class instance), logic engine (through an Interface class instance) and capacity for action (through a System class instance).
+    
+    Attributes:
+        memory: A Memory singleton instance
+        model: an Interface singleton instance
+        system: a System singleton instance
+        objective: the user's initially-stated stated objective
+    """
+
+
+    def __init__(self, args: argparse.Namespace) -> None:
+        """Initialization for the Agent class.
+        
+        Args:
+            args: Command-line arguments parsed by argparse
+        """
+
         # Create Memory singleton instance as attribute
         self.memory = Memory(args.model, args.output_dir)
         
@@ -36,13 +52,25 @@ class Agent():
         for p in op.prompts:
             self.memory.memorize(p['content'], p['role'])
 
-    def satisfied(self):
+
+    def satisfied(self) -> bool:
+        """Convenience method to check if execution loop should be stopped.
+        
+        If human and LLM are both satisfied, or if human asks for gracious halt to program, the final element of the agent's memory will be the stop message.
+        
+        returns:
+            boolean. True means stop execution.
+        """
+
         if "**STOP**" in self.memory.memories[-1].get('content', None):
             return True
         else:
             return False
 
-    def status_check(self):
+
+    def status_check(self) -> None:
+        """Print the latest additions to the agent's memory, as well as the current token usage count, to the console."""
+
         last_memory_ = self.memory.memories[-1]
         role_ = last_memory_['role']
         
