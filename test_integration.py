b82169fd0ebd7294f33580d164960c21ef57b4d9
Your Name <you@example.com>
2023-10-20 04:38:04 +0000

    Added integration test

--- test_integration.py
+++ test_integration.py
@@ -3,9 +3,8 @@ import pytest
 from Agent import Agent
 from khod_kaar import khod_kaar
 
-import subprocess
 
-class Args:
+class MockArgs:
     """Class to mock the argparser"""
 
     def __init__(self, model_, output_dir_, temperature_, autopilot_, objective_):
@@ -18,19 +17,23 @@ class Args:
 
 @pytest.fixture
 def simple_args():
+    """Fixture to mock simple arguments to create a program that writes 'Hello, World!'."""
+    
     args = ['gpt-4', 
             './output/', 
             0.0, 
             True, 
             "to write, test and execute a simple Python program that writes 'Hello, World!'."
     ]
-    return Args(*args)
+    return MockArgs(*args)
     
 
 def test_integration(simple_args):
+    """Integration test for khod-kaar using simple_args.
+    
+    khod-kaar must execute without errors, and the generated program must print 'Hello, World!'."""
 
-    assert True
-
-    # TODO This returns an ioctl error once the subprocess is activated. Investigate further.
-    # agent = Agent(simple_args)
-    # khod_kaar(agent)
+    khod_kaar(Agent(simple_args))
+    sp = subprocess.run(['python', '../hello_world_project/hello'],
+                       check=True, capture_output=True, text=True)
+    assert sp.stdout == 'Hello, World!'
